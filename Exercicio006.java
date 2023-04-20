6be1e85f9a1895a6d79e2817275ea2110ca016ae
Samuel Silva <105609567+Samuel77743@users.noreply.github.com>
2023-04-19 22:17:45 -0300

    Atualização de Exercícios

--- Exercicio006.java
+++ Exercicio006.java
@@ -1,5 +1,9 @@
 /**
  * Exercicio006
+ * Códigos bases para poder fazer entrada de dados:
+ * 1º -> import java.util.Scanner;
+ * 2º -> Scanner <nome> = new Scanner(System.in);
+ * 3º -> <tipo> <nome_variavel> = <nome>.next<tipo>();
  */
 import java.util.Scanner;
 
@@ -8,15 +12,28 @@ import java.util.Scanner;
         
         Scanner teclado = new Scanner(System.in);
         
-        System.out.print("Digite um número --> ");
-        int n1 = teclado.nextInt();
+        System.out.print("Digite o nome do aluno --> ");
+        String nome = teclado.nextLine();
 
-        System.out.print("Digite o segundo número --> ");
-        int n2 = teclado.nextInt();
+        System.out.print("Digite a nota da primeira prova --> ");
+        float nota1 = teclado.nextFloat();
 
-        int resposta = n1+n2;
-        System.out.printf("\nA soma de %d + %d = %d", n1, n2, resposta);
+        System.out.print("Digite a nota da segunda prova --> ");
+        float nota2 = teclado.nextFloat();
 
+        System.out.print("Digite a nota da terceira prova --> ");
+        float nota3 = teclado.nextFloat();
 
+        float media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.printf("\nNOTA DE %s:\nPROVA 1: %.2f\nPROVA 2: %.2f\nPROVA 3: %.2f", nome, nota1, nota2, nota3);
+        System.out.printf("\n\nMÉDIA ALCANÇADA --> %.2f", media);
+
+        final float mediaMin = 6; //Definindo constante
+
+        String situacao = (media >= mediaMin ? "Aprovado" : "Reprovado");
+        System.out.println("\nSituação: " + situacao);
+
+        teclado.close(); //Fechar entrada de dados
     }
  }
---
+++ Exercicio007.java
@@ -0,0 +1,12 @@
+/**
+ * Exercicio007
+ */
+public class Exercicio007 {
+    public static void main(String[] args) {
+        
+        //LOOP FOR
+        for(int count = 1; count <= 20; count++){
+            System.out.printf("%d\n", count);
+        }
+    }
+}
